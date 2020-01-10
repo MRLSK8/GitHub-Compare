64f85e466a14c7506ce34dafa563c4f981cd6efe
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-01-10 18:19:07 -0300

    Base do ambiente pronta!

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "./styles/globol";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App"></div>
+    </>
+  );
+}
+
+export default App;
